1fcf6537e573778c9cfa93f92fd8b310ef33fc09
DevBroParas <parasprofessionaly@gmail.com>
2025-08-01 23:27:57 +0530

    added meta data google

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -4,6 +4,10 @@ import { TimelineProvider } from "@/Context/TimelineContext";
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
+      <meta
+        name="google-site-verification"
+        content="SyN3eltW6eUyJTY2srkXjhZNEIoGijelsJxfCwVpIbY"
+      />
       <body>
         <TimelineProvider>{children}</TimelineProvider>
       </body>

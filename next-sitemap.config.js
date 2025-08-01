f8e892994bb3e7ed7254eae5c4053d174ac3210b
DevBroParas <parasprofessionaly@gmail.com>
2025-08-01 23:38:05 +0530

    aded sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,6 @@
+/** @type {import('next-sitemap').IConfig} */
+module.exports = {
+  siteUrl: 'https://yourdomain.com', // 👈 Replace with your actual domain
+  generateRobotsTxt: true,
+  generateIndexSitemap: false,
+}

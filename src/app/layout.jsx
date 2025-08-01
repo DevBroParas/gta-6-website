fb68ae8c202bcb8d99b568c58c7323027cfb01fa
DevBroParas <parasprofessionaly@gmail.com>
2025-08-01 23:56:27 +0530

    added metadata

--- src/app/layout.jsx
+++ src/app/layout.jsx
@@ -1,28 +1,24 @@
+// app/layout.js or layout.tsx
 import "./globals.css";
 import { TimelineProvider } from "@/Context/TimelineContext";
-import Head from "next/head";
+
+export const metadata = {
+  title: "GTA 6 Launch Showcase – devbro.site",
+  description:
+    "Unofficial GTA 6 launch website with slick animations. Built by Devbro using Next.js and GSAP.",
+  keywords: "GTA 6, devbro, gta6.devbro.site, Rockstar GTA6, Next.js project",
+  robots: "index, follow",
+  verification: {
+    google: "SyN3eltW6eUyJTY2srkXjhZNEIoGijelsJxfCwVpIbY",
+  },
+};
 
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-      <Head>
-        <title>GTA 6 Launch Showcase – devbro.site</title>
-        <meta
-          name="description"
-          content="Unofficial GTA 6 launch website with slick animations. Built by Devbro using Next.js and GSAP."
-        />
-        <meta name="robots" content="index, follow" />
-        <meta
-          name="keywords"
-          content="GTA 6, devbro, gta6.devbro.site, Rockstar GTA6, Next.js project"
-        />
-        <meta
-          name="google-site-verification"
-          content="SyN3eltW6eUyJTY2srkXjhZNEIoGijelsJxfCwVpIbY"
-        />
+      <head>
         <link rel="canonical" href="https://gta6.devbro.site" />
-      </Head>
-
+      </head>
       <body>
         <TimelineProvider>{children}</TimelineProvider>
       </body>
